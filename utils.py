435c6a4a5d5581a958c3936cdcebef9cc208a58b
Ian Vaz Araujo <ianaraujo15@gmail.com>
2024-08-31 22:42:20 -0300

    Exploratory data analysis with IBOV and CDI

---
+++ utils.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def historic_cdi(start: str = '01/07/1994', end: str = '01/07/2024') -> pd.Series:
+    """
+    Baixa os dados históricos do Banco Central em relação ao rendimento do CDI. 
+    """
+    api = f'https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=json&dataInicial={start}&dataFinal={end}'
+
+    response = pd.read_json(api)
+
+    cdi = response.set_index('data').squeeze()
+    cdi.index = pd.to_datetime(cdi.index, dayfirst=True)
+    
+    cdi_returns = (1 + (cdi / 100)).cumprod()
+
+    return cdi_returns
